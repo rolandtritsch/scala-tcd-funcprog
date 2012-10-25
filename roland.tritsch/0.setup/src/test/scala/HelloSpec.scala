ddea0857e293f6eab149c65888f6ca81b64d6997
Roland Tritsch <rtritsch@gilt.com>
2012-10-25 10:55:44 +0100

    Commit initial version of Setup

---
+++ roland.tritsch/0.setup/src/test/scala/HelloSpec.scala
@@ -0,0 +1,9 @@
+package ie.tcd.sccs.funcprog.setup
+
+import org.scalatest._
+
+class HelloSpec extends FlatSpec {
+  "Hello" should "allow you to increment a value by 1" in {
+    assert(Hello.incVal(1) === 1)
+  }
+}
