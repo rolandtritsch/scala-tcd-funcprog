80a316411b9ad91476391aa2141c07fa1778eb11
Roland Tritsch <rtritsch@gilt.com>
2012-10-30 12:20:16 +0000

    Set source code up for downloading/cloning

--- roland.tritsch/1.rewrite/scala/src/main/scala/Josephus.scala
+++ roland.tritsch/1.rewrite/scala/src/main/scala/Josephus.scala
@@ -1,31 +1,6 @@
 package ie.tcd.sccs.funcprog.josephus
 
 object Josephus {
-  def findSurvivor(numOfSoldiers: Int, skipping: Int): Int = {
-    assert(numOfSoldiers >= 1)
-    assert(skipping >= 0)
-
-    // val soldiers = Range(1, numOfSoldiers+1).toList
-    // val theRing = new RingIterator[Int](soldiers, soldiers.size-1)
-    val theRing = new RingIterator[Int]()
-    for(i <- 1 to numOfSoldiers) theRing.add(i)
-
-    theRing.reset(0)
-    while(theRing.size > 1) {
-      for(i <- 0 until skipping) theRing.advance
-      theRing.remove
-      // println(theRing.dump.mkString(","))
-    }
-
-    theRing.get
-  }
-
-  def main(args: Array[String]): Unit = {
-    assert(args.size == 2)
-
-    val numOfSoldiers = args(0).toInt
-    val killingEvery = args(1).toInt
-
-    println(findSurvivor(numOfSoldiers, killingEvery-1))
-  }
+    // TODO Implement solution here
+    // HINT Use RingIterator
 }
--- roland.tritsch/2.integrate/src/main/scala/Josephus.scala
+++ roland.tritsch/2.integrate/src/main/scala/Josephus.scala
@@ -1,25 +1,15 @@
 package ie.tcd.sccs.funcprog.josephus
 
-import org.apache.commons.collections.iterators.LoopingListIterator
-import java.util.{List => JList, LinkedList => JLinkedList}
-import java.lang.{Integer => JInteger}
+// TODO import Java classes
 
 object Josephus {
   def findSurvivor(numOfSoldiers: Int, skipping: Int): Int = {
-    assert(numOfSoldiers >= 1)
-    assert(skipping >= 0)
+    require(numOfSoldiers >= 1)
+    require(skipping >= 0)
 
-    val soldiers: JList[JInteger] = new JLinkedList[JInteger]()
-    for(i <- 1 to numOfSoldiers) soldiers.add(new JInteger(i))
-    val theRing = new LoopingListIterator(soldiers)
+    // TODO build linked list and put iterator on it
 
-    theRing.reset
-    while(theRing.size > 1) {
-      for(i <- 0 to skipping) theRing.next
-      theRing.remove
-    }
-
-    theRing.next.asInstanceOf[JInteger]
+    // TODO kill soldiers until one is left over and return it
   }
 
   def main(args: Array[String]): Unit = {
