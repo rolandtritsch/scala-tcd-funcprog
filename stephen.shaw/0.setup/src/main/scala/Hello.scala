d0d8f5594ee341b7a925898b7f4600b1776d2d56
Stephen Shaw <stephen.shaw@fieldaware.com>
2012-11-01 22:30:18 +0000

    Change Hello.scala to look at the first argument
    make it print "Hello " + firstArg

--- stephen.shaw/0.setup/src/main/scala/Hello.scala
+++ stephen.shaw/0.setup/src/main/scala/Hello.scala
@@ -2,7 +2,7 @@ package ie.tcd.sccs.funcprog.setup
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello World")
+    println("Hello " + args(0))
   }
 
   def incVal(i: Int): Int = {
