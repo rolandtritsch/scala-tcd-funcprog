c40ea857189e3d5960b4bacf4cea4eae67f3f370
Eoin <eoin@eoin-Satellite-Pro-L650.(none)>
2012-11-06 15:28:36 +0000

    Part 2

--- Eoin.Nolan/2.integrate/src/main/scala/Josephus.scala
+++ Eoin.Nolan/2.integrate/src/main/scala/Josephus.scala
@@ -1,6 +1,9 @@
 package ie.tcd.sccs.funcprog.josephus
 
 // TODO import Java classes
+import java.util.List
+import java.util.LinkedList
+import org.apache.commons.collections.iterators.LoopingListIterator
 
 object Josephus {
   def findSurvivor(numOfSoldiers: Int, skipping: Int): Int = {
@@ -8,8 +11,22 @@ object Josephus {
     require(skipping >= 0)
 
     // TODO build linked list and put iterator on it
+    val soldiers = new LinkedList();
+    val iterator = new LoopingListIterator(soldiers);
 
-    // TODO kill soldiers until one is left over and return it
+    Range(1, numOfSoldiers+1).foreach(iterator.add);
+    iterator.reset;
+
+
+  // TODO kill soldiers until one is left over and return it
+    while (iterator.size > 1) {
+      for (i <- 1 to skipping + 1){ 
+        iterator.next;
+      }
+      iterator.remove;
+    }
+
+    return iterator.next.asInstanceOf[Int];
   }
 
   def main(args: Array[String]): Unit = {
