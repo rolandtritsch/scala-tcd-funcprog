d87447dec6418adbcb9f36372982953e6873e53f
Stephen Shaw <stephen.shaw@fieldaware.com>
2012-11-06 15:30:43 +0000

    Add solution to lab 2

---
+++ stephen.shaw/2.integrate/src/main/scala/Josephus.scala
@@ -0,0 +1,45 @@
+package ie.tcd.sccs.funcprog.josephus
+
+// TODO import Java classes
+
+import org.apache.commons.collections.iterators.LoopingListIterator
+
+import java.util.List
+import java.util.LinkedList
+
+object Josephus {
+  def findSurvivor(numOfSoldiers: Int, skipping: Int): Int = {
+    require(numOfSoldiers >= 1)
+    require(skipping >= 0)
+
+    val theList = new LinkedList()
+    val iterator = new LoopingListIterator(theList)
+        
+    for (i <- 1 to numOfSoldiers) {
+        iterator.add(i)        
+    }
+
+    iterator.reset
+
+    while (iterator.size > 1) {
+        for (i <- 1 to skipping + 1) iterator.next
+        iterator.remove
+    }
+    
+    // Comes out as java.lang.Object
+    iterator.next.asInstanceOf[Int]
+
+    // TODO build linked list and put iterator on it
+
+    // TODO kill soldiers until one is left over and return it
+  }
+
+  def main(args: Array[String]): Unit = {
+    assert(args.size == 2)
+
+    val numOfSoldiers = args(0).toInt
+    val killingEvery = args(1).toInt
+
+    println(findSurvivor(numOfSoldiers, killingEvery-1))
+  }
+}
