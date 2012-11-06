abc5ea4c5d9556bef7952bee0fb5d710a35d3dd2
Shane Fitzpatrick <fitzpasd@gmail.com>
2012-11-06 13:39:54 +0000

    Test

--- shane.fitzpatrick/1.rewrite/scala/src/main/scala/Josephus.scala
+++ shane.fitzpatrick/1.rewrite/scala/src/main/scala/Josephus.scala
@@ -1,6 +1,19 @@
 package ie.tcd.sccs.funcprog.josephus
 
 object Josephus {
-    // TODO Implement solution here
-    // HINT Use RingIterator
+    def def main(args: Array[String]): Unit = {
+        assert(args.length === 2)
+
+        val numOfSoliders: Int = args(0)
+        val skipping: Int = args(1)
+
+        println(findSurvivor(numOfSoliders, skipping-1))  
+    }
+
+    def findSurvivor(numOfSoliders: Int, skipping: Int): {
+        theRing: List = Range(1, numOfSoliders+1).toList
+
+        
+
+    } 
 }
