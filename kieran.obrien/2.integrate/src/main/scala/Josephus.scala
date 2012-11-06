0054251faee6c4c64758ebe88e068ca39944488c
Krusedulle <kieran@ubuntu.ubuntu-domain>
2012-11-06 16:13:23 +0000

    lab 2 completed

--- kieran.obrien/2.integrate/src/main/scala/Josephus.scala
+++ kieran.obrien/2.integrate/src/main/scala/Josephus.scala
@@ -1,6 +1,9 @@
 package ie.tcd.sccs.funcprog.josephus
 
 // TODO import Java classes
+import java.util.LinkedList
+import java.util.{List => MyList}
+import org.apache.commons.collections.iterators.LoopingListIterator
 
 object Josephus {
   def findSurvivor(numOfSoldiers: Int, skipping: Int): Int = {
@@ -8,8 +11,16 @@ object Josephus {
     require(skipping >= 0)
 
     // TODO build linked list and put iterator on it
-
+    val ringOfSoldiers = new LoopingListIterator(new LinkedList)
+    for(i <- 1 to numOfSoldiers) ringOfSoldiers.add(i);
+    
     // TODO kill soldiers until one is left over and return it
+    while(ringOfSoldiers.size() > 1) {
+        for (i <- 0 to skipping)
+            ringOfSoldiers.next
+        ringOfSoldiers.remove
+    }
+    ringOfSoldiers.next.asInstanceOf[Int]
   }
 
   def main(args: Array[String]): Unit = {
