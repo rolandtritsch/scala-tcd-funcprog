85ced367bf0c4f80a651b62c04dab89fee75a0ec
Kieran <manningk@tcd.ie>
2012-11-06 16:12:46 +0000

    lab 2 compiling

--- kieran.manning/1.rewrite/scala/src/main/scala/Josephus.scala
+++ kieran.manning/1.rewrite/scala/src/main/scala/Josephus.scala
@@ -2,12 +2,29 @@ package ie.tcd.sccs.funcprog.josephus
 
 
 object Josephus {
-    def findSurivor(var numOfSolider: Int, var skipping: Int):Int = {
+
+
+    def findSurvivor(numOfSoldiers: Int, skipping: Int): Int = {
         require(numOfSoldiers >= 1)
         require(skipping >= 0)
 
-        var ringBuffer = ringBuffer[Int] = _
+        val ringBuffer = new RingIterator(Range(1, (numOfSoldiers + 1)).toList, 0)
         
-        for(i <- 0
+        ringBuffer.reset(0)
+
+        while(ringBuffer.size > 1){
+            for(i <- 0 until skipping) {
+                ringBuffer.advance
+            }
+            ringBuffer.remove
+        }
+        ringBuffer.get 
+    }
+
+    def main(args: Array[String]){
+        require(args.length == 2)
+        
+        println(findSurvivor(args(0).toInt, args(1).toInt - 1))
     }
 }
+
