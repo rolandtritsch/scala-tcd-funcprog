366f64af512295dea6be7e64f98d802b8673f8c6
Stephen Shaw <stephen.shaw@fieldaware.com>
2012-11-06 14:39:52 +0000

    Add solution to lab 1

--- stephen.shaw/1.rewrite/scala/src/main/scala/Josephus.scala
+++ stephen.shaw/1.rewrite/scala/src/main/scala/Josephus.scala
@@ -3,4 +3,31 @@ package ie.tcd.sccs.funcprog.josephus
 object Josephus {
     // TODO Implement solution here
     // HINT Use RingIterator
+    def main(args: Array[String]): Unit = {
+        val numOfSoldiers = args(0).toInt
+        val killingEvery = args(1).toInt
+        println(findSurvivor(numOfSoldiers, killingEvery-1))
+    }
+
+
+    def findSurvivor(numOfSoldiers: Int, skipping: Int): Int = {
+        val theRing = new RingIterator[Int]();
+
+        for(i <- 1 to numOfSoldiers) {
+            theRing.add(i)
+        }
+
+        theRing.advance
+
+
+        while (theRing.size > 1) {
+            for (j <- 1 to skipping) theRing.advance
+
+            theRing.remove
+        }
+
+        theRing.get
+    }
+
 }
+
